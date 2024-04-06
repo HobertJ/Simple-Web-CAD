7fc04338c7b11f1baacfab2bb113755ab0aa7e9a
HobertJ <13521079@std.stei.itb.ac.id>
2024-04-06 22:28:01 +0700

    feat: complete rectangle

--- src/2D-Shapes/rectangle.ts
+++ src/2D-Shapes/rectangle.ts
@@ -9,7 +9,6 @@ class Rectangle extends Shape implements Renderable, Transformable {
     public p2: Point;
     public p3: Point;
     public p4: Point;
-    
     public center: Point;
     public tx: number;
     public ty: number;
@@ -37,19 +36,23 @@ class Rectangle extends Shape implements Renderable, Transformable {
     }
 
     public getCenter(): Point {
-        return this.center;
+        const [p1x, p1y] = this.p1.getPair();
+        const [p2x, p2y] = this.p2.getPair();
+        const [p3x, p3y] = this.p3.getPair();
+        const [p4x, p4y] = this.p4.getPair();
+
+        const centerX = (p1x + p2x + p3x + p4x) / 4;
+        const centerY = (p1y + p2y + p3y + p4y) / 4;
+        
+        return new Point([centerX, centerY]);
     }
 
     public isDrawable(): boolean {
         return this.p3 != null;
     }
 
-    public fixateDrawing(point: Point): void {
-        this.center = point;
-    }
-
     public drawMethod(gl: WebGLRenderingContext): number {
-        return 0;
+        return gl.TRIANGLE_FAN;
     }
 
     public getNumberOfVertices(): number {
@@ -57,10 +60,36 @@ class Rectangle extends Shape implements Renderable, Transformable {
     }
 
     public addPosition(gl: WebGLRenderingContext): void {
-        return;
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array([
+                ...this.p1.getPair(),
+                ...this.p2.getPair(),
+                ...this.p3.getPair(),
+                ...this.p4.getPair(),
+                ...this.p1.getPair(),
+            ]),
+            gl.STATIC_DRAW
+        )
     }
 
     public addColor(gl: WebGLRenderingContext): void {
-        return;
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array([
+                ...this.p1.getColor(),
+                ...this.p2.getColor(),
+                ...this.p3.getColor(),
+                ...this.p4.getColor(),
+                ...this.p1.getColor(),
+            ]),
+            gl.STATIC_DRAW
+        );
+    }
+
+    public draw(point: Point): void {
+        this.p3 = point;
+        this.p2 = new Point([this.p1.x, this.p3.y]);
+        this.p4 = new Point([this.p3.x, this.p1.y]);
     }
 }
