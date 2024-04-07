f6026c629594ff42cc8762a1fe789e98ff494073
melvinkj <melvinkentj@gmail.com>
2024-04-07 17:07:25 +0700

    feat: renderAll function

---
+++ src/Functions/render-all.ts
@@ -0,0 +1,15 @@
+import Shape from "Main/2D-Shapes/shape";
+import { render } from "./render";
+import ProgramInfo from "./program-info.interface";
+import Renderable from "Main/2D-Shapes/Interfaces/renderable.interface";
+import Transformable from "Main/2D-Shapes/Interfaces/transformable.interface";
+
+export function renderAll(gl: WebGLRenderingContext, programInfo: ProgramInfo, shapes: (Renderable&Transformable)[]): void {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  
+    for (const shape of shapes) {
+      render(gl, programInfo, shape)
+    }
+  
+    window.requestAnimationFrame(() => renderAll(gl, programInfo, shapes));
+  };
